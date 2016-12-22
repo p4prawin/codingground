b5c228e0005e52aba8f2db80dbacca348124f5b8
Pravin Mane <pra.win.mane@gmail.com>
2016-12-22 11:42:42 +0530

    Created by Coding Ground Client

---
+++ New Project-20161222/main.py
@@ -0,0 +1,27 @@
+
+
+
+
+
+
+
+# Hello World program in Python
+'''
+a,b=7,'8'
+print a+int(b)
+
+user_val = input("enter value ")
+print ("user input ",user_val)
+user_int = int(user_val)
+print ("user input int",user_int)
+
+
+    
+myString = "asfdsfadsf\n"
+print myString
+a = 1.1121
+print("value of a=",a)
+
+print("String : %s , int %d\n"%("faaaaa", 23123) )
+
+'''
